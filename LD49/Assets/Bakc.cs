0be64f28fcf87bda2cc3f8909d51192095c4c14d
DouglasKotsubo <dkslenderman@gmail.com>
2021-10-02 14:55:21 -0300

    back

---
+++ LD49/Assets/Bakc.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Bakc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Retorno()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
