31c09072f27748b3fe1dd47007ef288759cf6a4e
Atlaswe <galtmerklein@gmail.com>
2021-10-03 16:52:11 -0300

    Added some things in main menu
    Controls and a cool background

--- LD49/Assets/Iniciar.cs
+++ LD49/Assets/Iniciar.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Iniciar : MonoBehaviour
 {
+    public Animator anim;
+    bool clicked = false;
     public void Começar()
     {
+        if (!clicked) StartCoroutine(FadeScreen());
+        clicked = true;
+    }
+
+    IEnumerator FadeScreen(){
+        anim.gameObject.SetActive(true);
+        anim.SetTrigger("go");
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene("SampleScene");
     }
 }
