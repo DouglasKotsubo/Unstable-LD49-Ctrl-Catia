72616169bd3d9b0f028b9b29f2d996d819f148c4
DouglasKotsubo <dkslenderman@gmail.com>
2021-10-03 13:17:40 -0300

    lightsactivateF

--- LD49/Assets/Lights1.cs
+++ LD49/Assets/Lights1.cs
@@ -1,29 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Lights1 : MonoBehaviour
 {
+    public Light2D lights;
     public GameObject Player;
-    public int distance;
+    public float value;
     Vector3 s0;
     // Start is called before the first frame update
     void Start()
     {
-        s0 = Player.transform.position + distance * new Vector3 (1,0,0);
-        transform.position = s0;
+
     }
 
     // Update is called once per frame
     //Arrumar a direção
     void Update()
     {
-        transform.position = Player.transform.position + distance * new Vector3 (1,0,0);
+        if (Input.GetKeyDown("f"))
+        {
+            if (lights.intensity == 0f)
+            {
+                lights.intensity = value;
+            }
+            else
+            {
+                lights.intensity = 0f;
+            }
+        }
+        transform.position = Player.transform.position;
         if (Input.GetKey("d"))
         {transform.eulerAngles = new Vector3(0,0,0);}
         if (Input.GetKey("a"))
-        {transform.eulerAngles = new Vector3(0,0,180);
-        transform.position = Player.transform.position + distance * new Vector3 (0,1,0);}
+        {transform.eulerAngles = new Vector3(0,0,180);}
         if (Input.GetKey("s"))
         {transform.eulerAngles = new Vector3(0,0,-90);}
         if (Input.GetKey("w"))
