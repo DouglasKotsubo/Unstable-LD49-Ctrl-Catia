c09a354d93c001842adfd2902f775070612ff1e0
Atlaswe <galtmerklein@gmail.com>
2021-10-03 12:38:54 -0300

    Scene 1 almost done

--- LD49/Assets/Scripts/Cena1Script.cs
+++ LD49/Assets/Scripts/Cena1Script.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Cena1Script : MonoBehaviour
 {
     public PlayerMove playerMotion;
     public GameObject playerLight;
     public DialogueManager manager;
+    public GameObject door;
+
+    [Header("Dialogos")]
     public DialogueClass dialogoInicial;
+    public DialogueClass dpsDoSusto;
+    public DialogueClass flashlightDialogue;
+
+    [Header("Medo")]
     public ScaryTexturing st;
     public GameObject[] tilemaps;
 
@@ -41,8 +49,52 @@ public class Cena1Script : MonoBehaviour
             }
         }
         else if (progress == 2){
+            playerMotion.freezed = true;
+            manager.StartDialogue(flashlightDialogue);
+            manager.ShowNextDialogue();
+            progress++;
+        }
+        else if (progress == 3){
+            if (Input.GetKeyDown(KeyCode.E) && !(manager.sentences.Count == 0 && !manager.going)){
+                manager.ShowNextDialogue();
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && (manager.sentences.Count == 0 && !manager.going)){
+                manager.ShowNextDialogue();
+                progress++;
+                playerMotion.freezed = false;
+            }
+        }
+        else if (progress == 4){
             StartCoroutine(WaitForIt());
             progress++;
+        }
+        else if (progress == 6){
+            playerMotion.freezed = true;
+            manager.StartDialogue(dpsDoSusto);
+            manager.ShowNextDialogue();
+            progress++;
+        }
+        else if (progress == 7){
+            if (Input.GetKeyDown(KeyCode.E) && !(manager.sentences.Count == 0 && !manager.going)){
+                manager.ShowNextDialogue();
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && (manager.sentences.Count == 0 && !manager.going)){
+                manager.ShowNextDialogue();
+                progress++;
+                playerMotion.freezed = false;
+            }
+        }
+        else if (progress == 8){
+            door.SetActive(true);
+            progress++;
+        }
+        else if (progress == 9){
+            if (Input.GetKeyDown(KeyCode.E)){
+                Vector3 distancia = playerMotion.gameObject.GetComponent<Transform>().position - door.GetComponent<Transform>().position;
+                if (distancia.magnitude <= 1.5f){
+                    SceneManager.LoadScene("Cena2");
+                }
+            }
         }
     }
 
@@ -53,5 +105,11 @@ public class Cena1Script : MonoBehaviour
             st.ScaryMaterial(obj, 1);
         }
         st.ScaryPostProcessing();
+        yield return new WaitForSeconds(1.5f);
+        foreach (GameObject obj in tilemaps){
+            st.NormalMaterial(obj);
+        }
+        yield return new WaitForSeconds(1);
+        progress++;
     }
 }
